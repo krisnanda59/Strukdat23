5258387955e1fd05d8ce9dbef35cacbd99372c2b
Krisnanda Utama <krisnanda59@gmail.com>
2023-10-11 21:47:38 +0700

    Create Made Krisnanda Utama_05311840000033.cpp

---
+++ Tugas/Struct/Made Krisnanda Utama_05311840000033.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+struct UserData {
+    std: :string name;
+    int nrp;
+};
+
+const int MAX_DATA = 10;
+int numData = 10;
+UserData dataArr[MAX_DATA] = {
+    {"",},
+    {"",},
+    {"",},
+    {"",},
+    {"",},
+    {"",},
+    {"",},
+    {"",},
+    {"",},
+    {"",}
+}
